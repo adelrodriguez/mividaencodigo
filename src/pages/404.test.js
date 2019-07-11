74b5bdeaa2a14e2cf52c49177c261e894f82e584
Adel Rodríguez <adelrodriguezv@gmail.com>
2019-07-10 23:26:43 -0400

    Add 404 page

--- src/pages/404.test.js
+++ src/pages/404.test.js
@@ -2,17 +2,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { useStaticQuery, StaticQuery } from 'gatsby';
-import NotFoundTemplate from './not-found-template';
+import NotFoundTemplate from './404';
 import siteMetadata from '../../jest/__fixtures__/site-metadata';
 import type { RenderCallback } from '../types';
 
 describe('NotFoundTemplate', () => {
   beforeEach(() => {
     StaticQuery.mockImplementationOnce(
-      ({ render }: RenderCallback) => (
-        render(siteMetadata)
-      ),
-      useStaticQuery.mockReturnValue(siteMetadata)
+      ({ render }: RenderCallback) => render(siteMetadata),
+      useStaticQuery.mockReturnValue(siteMetadata),
     );
   });
 
