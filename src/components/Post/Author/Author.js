71286464f76ba37ed8d23a380ce84954e66ecd96
Adel Rodríguez <adelrodriguezv@gmail.com>
2019-08-03 11:57:32 -0400

    Add new author photo and contact info

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,14 +1,28 @@
 // @flow
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
 const Author = () => {
   const { author } = useSiteMetadata();
+  const { photo } = useStaticQuery(
+    graphql`
+      {
+        photo: imageSharp(original: { src: { regex: "/adel/" } }) {
+          fixed(height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    `,
+  );
 
   return (
     <div className={styles['author']}>
+      <Image fixed={photo.fixed} className={styles['about__logo']} alt={author.name} />
       <p className={styles['author__bio']}>
         {author.bio}
         <a
--- src/utils/get-contact-href.js
+++ src/utils/get-contact-href.js
@@ -4,22 +4,22 @@ const getContactHref = (name: string, contact: string) => {
 
   switch (name) {
     case 'twitter':
-      href = `https://www.twitter.com/${contact}`;
+      href = `https://www.twitter.com/${contact}/`;
       break;
     case 'github':
-      href = `https://github.com/${contact}`;
+      href = `https://github.com/${contact}/`;
       break;
     case 'facebook':
-      href = `https://www.facebook.com/${contact}`;
+      href = `https://www.facebook.com/${contact}/`;
       break;
     case 'instagram':
-      href = `https://www.instagram.com/${contact}`;
+      href = `https://www.instagram.com/${contact}/`;
       break;
     case 'email':
       href = `mailto:${contact}`;
       break;
     case 'linkedin':
-      href = `https://www.linkedin.com/in/${contact}`;
+      href = `https://www.linkedin.com/in/${contact}/`;
       break;
     default:
       href = contact;
