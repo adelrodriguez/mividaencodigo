a8cf95e0172d747d4b1395f3bee1f84b2742fc4f
Adel Rodríguez <adelrodriguezv@gmail.com>
2020-02-07 15:31:50 -0400

    fix: correct spacing in Progress

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -8,23 +8,28 @@ type Props = {
 
 const Progress = ({ edges }: Props) => (
   <div className={styles['progress']}>
-    <h2 className={styles['progress__title']}>¿En qué estoy?</h2>
+    <h2 className={styles['progress__title']}>¿Qué estoy aprendiendo?</h2>
     <p>
-      El propósito de este blog es darle seguimiento a mi crecimiento como ingeniero de software. Y
-      eso significa mucho estudiar. Estos son los cursos que estoy tomando actualmente:
+      Uno de los propósitos de este blog es darle seguimiento a mi crecimiento como Ingeniero de
+      Foftware. Y eso significa mucho estudiar, y un aprendizaje constante. Estos son los cursos que
+      estoy tomando actualmente:
     </p>
     <ul>
       {edges.map((edge) => (
         <li key={edge.node.id}>
           <div className={styles['progress__course']}>
-            <a className={styles['progress__course-name']} href={edge.node.url} target='_blank'>
-              {edge.node.name}
-            </a>
-            {edge.node.labels.map((label) => (
-              <span className={styles['progress__course-label']} key={label.id}>
-                {label.name}
-              </span>
-            ))}
+            <div>
+              <a className={styles['progress__course-name']} href={edge.node.url} target='_blank'>
+                {edge.node.name}
+              </a>
+            </div>
+            <div className={styles['progress__course-labels']}>
+              {edge.node.labels.map((label) => (
+                <span className={styles['progress__course-label']} key={label.id}>
+                  {label.name}
+                </span>
+              ))}
+            </div>
           </div>
         </li>
       ))}
